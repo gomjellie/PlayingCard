339b2a606cbc2f46179947b91c2acc6acb838693
gomjellie <gomjellie@gmail.com>
2021-01-14 08:44:43 +0900

    제스처를 인식하는 카드 View

---
+++ PlayingCard/PlayingCard.swift
@@ -0,0 +1,70 @@
+//
+//  PlayingCard.swift
+//  PlayingCard
+//
+//  Created by Jacky on 2021/01/09.
+//
+
+import Foundation
+
+struct PlayingCard: CustomStringConvertible
+{
+    var description: String {
+        return "\(suit) \(rank)"
+    }
+    
+    var suit: Suit
+    var rank: Rank
+    
+    enum Suit: String, CustomStringConvertible {
+        var description: String {
+            return self.rawValue
+        }
+        
+        case spades = "♠️"
+        case hearts = "♥️"
+        case diamonds = "♦️"
+        case clubs = "♣️"
+        
+        static var all: [Suit] = [
+            .spades, .hearts, .diamonds, .clubs
+        ]
+    }
+    
+    enum Rank: CustomStringConvertible {
+        var description: String {
+            switch self {
+            case .ace: return "A"
+            case .numeric(let pips): return String(pips)
+            case .face(let kind): return kind
+            }
+        }
+        
+        case ace
+        case face(String)
+        case numeric(Int)
+        
+        var order: Int {
+            switch self {
+            case .ace: return 1
+            case .numeric(let pips): return pips
+            case .face(let kind) where kind == "J": return 11
+            case .face(let kind) where kind == "Q": return 12
+            case .face(let kind) where kind == "K": return 13
+            default: return 0
+            }
+        }
+        
+        static var all: [Rank] {
+            var allRanks = [Rank.ace]
+            
+            
+            for pips in 2...10 {
+                allRanks.append(Rank.numeric(pips))
+            }
+            
+            allRanks += [.face("J"), .face("Q"), .face("K")]
+            return allRanks
+        }
+    }
+}
---
+++ PlayingCard/PlayingCardDeck.swift
@@ -0,0 +1,28 @@
+//
+//  PlayingCardDeck.swift
+//  PlayingCard
+//
+//  Created by Jacky on 2021/01/09.
+//
+
+import Foundation
+
+struct PlayingCardDeck
+{
+    private(set) var cards = [PlayingCard]()
+    
+    init() {
+        for suit in PlayingCard.Suit.all {
+            for rank in PlayingCard.Rank.all {
+                cards.append(PlayingCard(suit: suit, rank: rank))
+            }
+        }
+    }
+    
+    mutating func draw() -> PlayingCard? {
+        if cards.count > 0 {
+            return cards.remove(at: 0)
+        }
+        return nil
+    }
+}
